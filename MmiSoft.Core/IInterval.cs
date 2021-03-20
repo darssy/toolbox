7ee17c344bc15a7e9096b081bfc73d71f7677720
adamstyl <adamastyl@gmail.com>
2021-03-20 01:59:35 +0100

    Improvements in IInterval

--- MmiSoft.Core/IInterval.cs
+++ MmiSoft.Core/IInterval.cs
@@ -4,10 +4,10 @@ namespace MmiSoft.Core
 {
 	public interface IInterval<T>
 	{
+		bool IsBefore(T value);
+		bool IsAfter(T value);
 		bool Contains(T value);
 		T Closest(T value);
-		//Temporary value until altitude selection is finalized
-		T Lower {get;}
 	}
 
 	public abstract class InfiniteEndpointInterval<T> : IInterval<T> where T: IComparable<T>
@@ -23,14 +23,14 @@ namespace MmiSoft.Core
 
 		public T Endpoint => endpoint;
 
+		public abstract bool IsBefore(T value);
+		public abstract bool IsAfter(T value);
 		public abstract bool Contains(T value);
 
 		public virtual T Closest(T value)
 		{
 			return Contains(value) ? value : Endpoint;
 		}
-
-		public T Lower => endpoint;
 
 		public override string ToString()
 		{
@@ -40,8 +40,11 @@ namespace MmiSoft.Core
 
 	public class LessThanInterval<T> : InfiniteEndpointInterval<T> where T: IComparable<T>
 	{
-		public LessThanInterval(T endpoint, string format="[-\u221E,{0}]") : base(endpoint, format)
+		public LessThanInterval(T endpoint, string format="(-\u221E,{0}]") : base(endpoint, format)
 		{ }
+
+		public override bool IsBefore(T value) => false;
+		public override bool IsAfter(T value) => !Contains(value);
 
 		public override bool Contains(T value)
 		{
@@ -52,8 +55,11 @@ namespace MmiSoft.Core
 	public class GreaterThanInterval<T> : InfiniteEndpointInterval<T> where T: IComparable<T>
 	{
 
-		public GreaterThanInterval(T endpoint, string format="[{0},\u221E]") : base(endpoint, format)
+		public GreaterThanInterval(T endpoint, string format="[{0},\u221E)") : base(endpoint, format)
 		{ }
+
+		public override bool IsBefore(T value) => !Contains(value);
+		public override bool IsAfter(T value) => false;
 
 		public override bool Contains(T value)
 		{
@@ -74,6 +80,17 @@ namespace MmiSoft.Core
 			this.format = format;
 		}
 
+		public BoundedInterval(T lower, T upper, string format="[{0},{1}]")
+		{
+			if (lower.CompareTo(upper) > 0) Util.Swap(ref lower, ref upper);
+			this.lower = new GreaterThanInterval<T>(lower);
+			this.upper = new LessThanInterval<T>(upper);
+			this.format = format;
+		}
+
+		public bool IsBefore(T value) => lower.IsBefore(value);
+		public bool IsAfter(T value) => upper.IsAfter(value);
+
 		public bool Contains(T value)
 		{
 			return lower.Contains(value) && upper.Contains(value);
@@ -86,28 +103,33 @@ namespace MmiSoft.Core
 			return value;
 		}
 
-		public T Lower => lower.Endpoint;
-
 		public override string ToString()
 		{
 			return string.Format(format, lower.Endpoint, upper.Endpoint);
 		}
 	}
 
-	public class DegenerateInterval<T> : InfiniteEndpointInterval<T> where T: IComparable<T>
+	public class DegenerateInterval<T> : IInterval<T> where T: IComparable<T>
 	{
-		public DegenerateInterval(T endpoint, string format="[{0},{0}]") : base(endpoint, format)
+		private T endpoint;
+		private string format;
+
+		public DegenerateInterval(T endpoint, string format="[{0},{0}]")
 		{
+			this.endpoint = endpoint;
+			this.format = format;
 		}
 
-		public override bool Contains(T value)
-		{
-			return value.CompareTo(Endpoint) == 0;
-		}
+		public bool IsBefore(T value) => endpoint.CompareTo(value) < 0;
+		public bool IsAfter(T value) => endpoint.CompareTo(value) > 0;
 
-		public override T Closest(T value)
+		public bool Contains(T value) => endpoint.CompareTo(value) == 0;
+
+		public T Closest(T value) => endpoint;
+
+		public override string ToString()
 		{
-			return Endpoint;
+			return string.Format(format, endpoint);
 		}
 	}
 }
