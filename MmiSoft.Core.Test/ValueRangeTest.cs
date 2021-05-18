94fb720686c48f8939caa049755c177c9f7cad32
adamstyl <adamastyl@gmail.com>
2021-05-18 14:46:55 +0200

    Added documentation and null checks in ValueRange; created test cases

---
+++ MmiSoft.Core.Test/ValueRangeTest.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+
+namespace MmiSoft.Core.Test
+{
+	[TestFixture]
+	public class ValueRangeTest
+	{
+		[Test]
+		public void Intersects_NonOverlappingRanges_ReturnsFalse()
+		{
+			Assert.False(new ValueRange<int>(5, 10).Intersects(new ValueRange<int>(11, 13)));
+		}
+
+		[Test]
+		public void Intersects_EngulfedRange_ReturnsTrue()
+		{
+			Assert.True(new ValueRange<int>(5, 10).Intersects(new ValueRange<int>(6, 7)));
+		}
+
+		[Test]
+		public void Intersects_OuterRangeRange_ReturnsFalse()
+		{
+			Assert.False(new ValueRange<int>(6, 7).Intersects(new ValueRange<int>(5, 10)));
+		}
+
+		[Test]
+		public void Intersects_TouchingRange_ReturnsTrue()
+		{
+			Assert.True(new ValueRange<int>(1, 5).Intersects(new ValueRange<int>(5, 10)));
+			Assert.True(new ValueRange<int>(1, 5).Intersects(new ValueRange<int>(-5, 1)));
+		}
+
+		[Test]
+		public void Intersects_NullRange_ReturnsFalse()
+		{
+			Assert.False(new ValueRange<int>(1, 5).Intersects(null));
+		}
+
+		[Test]
+		public void Intersects_SameReferenceRange_ReturnsTrue()
+		{
+			ValueRange<int> range = new ValueRange<int>(1, 5);
+			Assert.True(range.Intersects(range));
+		}
+
+		[Test]
+		public void Constructor_TransposedValues_CreatesObjectWithMinLtMax()
+		{
+			ValueRange<int> range = new ValueRange<int>(13, 2);
+			Assert.AreEqual(2, range.MinValue);
+			Assert.AreEqual(13, range.MaxValue);
+		}
+	}
+}
--- MmiSoft.Core/ValueRange.cs
+++ MmiSoft.Core/ValueRange.cs
@@ -2,13 +2,25 @@
 
 namespace MmiSoft.Core
 {
+	/// <summary>
+	/// A simple value range between 2 comparable values. Not to be confused with the C# 8 ranges API.
+	/// </summary>
+	/// <typeparam name="T">Any type that implements Comparable</typeparam>
 	public sealed class ValueRange<T> where T : IComparable<T>
 	{
 		private T minValue;
 		private T maxValue;
 
+		/// <summary>
+		/// Constructs a new Range with specific min and max. If max &lt; min then min and max are swapped.
+		/// </summary>
+		/// <param name="min">Lowest bound of range, inclusive</param>
+		/// <param name="max">highest bound of range, inclusive</param>
+		/// <exception cref="ArgumentNullException">If min or max is null</exception>
 		public ValueRange(T min, T max)
 		{
+			if (min == null) throw new ArgumentNullException(nameof(min));
+			if (max == null) throw new ArgumentNullException(nameof(max));
 			if (max.CompareTo(min) < 0)
 			{
 				Util.Swap(ref min, ref max);
@@ -21,11 +33,21 @@ namespace MmiSoft.Core
 
 		public T MaxValue => maxValue;
 
+		/// <summary>
+		/// Checks if the argument is between the min and max of this range, bounds are included.
+		/// </summary>
+		/// <param name="value">The value to check</param>
+		/// <returns>True if value &lt;= max and value &gt;= min, false otherwise</returns>
 		public bool IsInRange(T value)
 		{
 			return minValue.CompareTo(value) <= 0 && maxValue.CompareTo(value) >= 0;
 		}
 
+		/// <summary>
+		/// Checks if this range intersects with another.
+		/// </summary>
+		/// <param name="range"></param>
+		/// <returns>True if either min or max of range are between min and max of this range. False otherwise</returns>
 		public bool Intersects(ValueRange<T> range)
 		{
 			if (range == null) return false;
