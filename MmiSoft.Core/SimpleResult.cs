720a12a2a176e8f593b13108ee3908b6caeaa9cf
adamstyl <adamastyl@gmail.com>
2021-11-19 13:28:19 +0200

    Added SimpleResult class for methods that want to return success or failure without the burden of throwing an exception

---
+++ MmiSoft.Core/SimpleResult.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace MmiSoft.Core
+{
+	/// <summary>
+	/// Operation result without a value and only an error message.
+	/// </summary>
+	public class SimpleResult
+	{
+		public static readonly SimpleResult Success = new SimpleResult("");
+
+		private readonly string errorMessage;
+
+		public SimpleResult(string errorMessage)
+		{
+			this.errorMessage = errorMessage ?? throw new ArgumentNullException(nameof(errorMessage));
+		}
+
+		public bool Succeeded => errorMessage == "";
+
+		public override string ToString()
+		{
+			return Succeeded ? "(Success)" : errorMessage;
+		}
+	}
+}
