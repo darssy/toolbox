236c133cacfd630eb4c647e03408a921625060a6
adamstyl <adamastyl@gmail.com>
2022-05-11 21:31:31 +0200

    Fixed documentation comment in EventLogger and added one for ValueEventArgs

--- MmiSoft.Core/EventLogger.cs
+++ MmiSoft.Core/EventLogger.cs
@@ -3,29 +3,27 @@ using MmiSoft.Core.Logging;
 
 namespace MmiSoft.Core
 {
-	/**
-	 * <para>
-	 * A bit of history behind this class: many years ago it was my primary logging class. Quite primitive but got the job
-	 * done. At some point and when the application that I was using it in became more serious, I converted it as a wrapper
-	 * for NLog. The decision to make this class static (btw Mark Seemann calls that "Ambient Context" in his book) made
-	 * the whole transition so smooth that I dare to call it a B E A U T Y.
-	 * </para>
-	 *
-	 * <para>
-	 * Fast forward to today, where this class is part of an open source library, hard coding NLog in this static wrapper
-	 * might not be very handy as it forces you to get onboard a logging library you might not want to use. In the first
-	 * phase of refactorings this class will be converted to a shell for an NLog implementation. In the second and final
-	 * one, it will be a wrapper for a simple console logging mechanism and NLog will be removed from the dependency list.
-	 * </para>
-	 *
-	 * <para>
-	 * The benefit of using this class will be that you could access it from everywhere without wondering "how". Just as
-	 * you do <c>Environment.NewLine</c> you could do <c>EventLogger.Log()</c>. Then if for any reason you want to change
-	 * logging library, you could do it in one place and then set the <c>LoggerImplementation</c> property. Alternatively,
-	 * if you are hard core into constructor injection, you could pass <c>ILogWrapper</c> to <i>every</i> class that needs
-	 * to do logging -which IMHO is an overkill.
-	 * </para>
-	 */
+	/// <summary>
+	/// <para>
+	/// A bit of history behind this class: many years ago it was my primary logging class. Quite primitive but got the job
+	/// done. At some point and when the application that I was using it in became more serious, I converted it as a wrapper
+	/// for NLog. The decision to make this class static (btw Mark Seemann calls that "Ambient Context" in his book) made
+	/// the whole transition so smooth that I dare to call it a B E A U T Y.
+	/// </para>
+	/// <para>
+	/// Fast forward to today, where this class is part of an open source library, hard coding NLog in this static wrapper
+	/// might not be very handy as it forces you to get onboard a logging library you might not want to use. In the first
+	/// phase of refactorings this class will be converted to a shell for an NLog implementation. In the second and final
+	/// one, it will be a wrapper for a simple console logging mechanism and NLog will be removed from the dependency list.
+	/// </para>
+	/// <para>
+	/// The benefit of using this class will be that you could access it from everywhere without wondering "how". Just as
+	/// you do <c>Environment.NewLine</c> you could do <c>EventLogger.Log()</c>. Then if for any reason you want to change
+	/// logging library, you could do it in one place and then set the <c>LoggerImplementation</c> property. Alternatively,
+	/// if you are hard core into constructor injection, you could pass <c>ILogWrapper</c> to <i>every</i> class that needs
+	/// to do logging -which IMHO is an overkill.
+	/// </para>
+	/// </summary>
 	public static class EventLogger
 	{
 		private static ILogWrapper loggerImplementation = new NLogWrapper();
--- MmiSoft.Core/ValueEventArgs.cs
+++ MmiSoft.Core/ValueEventArgs.cs
@@ -4,6 +4,16 @@ namespace MmiSoft.Core
 {
 	public delegate void ValueEventHandler<T>(object sender, ValueEventArgs<T> e);
 
+	/// <summary>
+	/// There was a time that you <i>had</i> to extend EventArgs. According to Microsoft, this isn't necessary anymore
+	/// https://docs.microsoft.com/en-us/dotnet/csharp/modern-events (I hope that this stays alive for some time) and
+	/// you can pass whatever type you like in lieu of a derivative of EventArgs.
+	/// <para>
+	/// This class is from that time. Use it if you need to pass a value to the event subscribers and you need to extend
+	/// EventArgs at the same time (for any reason).
+	/// </para>
+	/// </summary>
+	/// <typeparam name="T">The type of the value to pass to the subscriber</typeparam>
 	[Serializable]
 	public class ValueEventArgs<T> : EventArgs
 	{
