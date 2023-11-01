4352e3b9ad560e70dc186ca8ed31b4fbe27ff1a5
adamstyl <adamastyl@gmail.com>
2023-11-02 00:36:46 +0100

    Added SelectableItemWrapper class

---
+++ MmiSoft.Core/ComponentModel/SelectableItemWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MmiSoft.Core.ComponentModel
+{
+	/// <summary>
+	/// Class for wrapping IDesignatedItems where SelectableItemBase cannot be directly inherited eg when the item is a Control.
+	/// </summary>
+	[Serializable]
+	public class SelectableItemWrapper : SelectableItemBase
+	{
+		protected readonly IDesignatedItem wrapped;
+
+		[field: NonSerialized]
+		public override event EventHandler SelectedChanged;
+
+		public SelectableItemWrapper(IDesignatedItem wrapped)
+			:base(wrapped?.Designation ?? throw new ArgumentNullException(nameof(wrapped)))
+		{
+			this.wrapped = wrapped;
+		}
+
+		/// <summary>
+		/// Triggers the SelectedChanged event. The sender is not "this" but the object wrapped in this wrapper.
+		/// </summary>
+		/// <param name="e">An EventArgs that contains the event data.</param>
+		protected override void OnSelectedChanged(EventArgs e)
+		{
+			SelectedChanged?.Invoke(wrapped, e);
+		}
+	}
+}
